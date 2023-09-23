c46b3960a7321ee11a8c09e76200b650e001fa57
ingridmidia <ingridmidia@gmail.com>
2023-09-23 11:01:00 -0500

    added allQuestions array and showQuestion fucntion

--- script/script.js
+++ script/script.js
@@ -2,12 +2,17 @@ var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
 var question = document.getElementById("question");
 var initialPage = document.getElementById("initial-page");
+var questionTitle = document.getElementById("question-title");
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
 
 startButton.addEventListener("click", function (e) {
     setTimer();
     initialPage.style.display = "none";
     question.style.display = "block";
-    // showQuestion();
+    showQuestion();
 });
 
 var timeLeft = 75;
@@ -22,3 +27,31 @@ function setTimer() {
         }
     }, 1000);
 }
+
+var allQuestions = [
+    {
+        questionTitle: "Which of the following is not a primitive data type in JavaScript?",
+        answers: ["1. number", "2. boolean", "3. object", "4. string"],
+        // rightAnswer: 2
+    },
+    {
+        questionTitle: "Which method is used to remove the last element from an array and return it?",
+        answers: ["1. pop", "2. slice", "3. push", "4. concat"],
+        // rightAnswer: 1
+    }
+
+];
+
+var currentQuestion = 0;
+
+function showQuestion() {
+
+    questionTitle.textContent = allQuestions[currentQuestion].questionTitle;
+    answer1.textContent = allQuestions[currentQuestion].answers[0];
+    answer2.textContent = allQuestions[currentQuestion].answers[1];
+    answer3.textContent = allQuestions[currentQuestion].answers[2];
+    answer4.textContent = allQuestions[currentQuestion].answers[3];
+
+}
+
+// currentQuestion++;//when button is pressed
