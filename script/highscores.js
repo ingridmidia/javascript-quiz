2cd334bd7c3a3fa6f4879a375eb66b6f977613ea
ingridmidia <ingridmidia@gmail.com>
2023-09-24 18:43:22 -0500

    added highscores list

--- script/highscores.js
+++ script/highscores.js
@@ -1,5 +1,21 @@
 var goBackButton = document.getElementById("go-back-button");
+var highscoresList = document.getElementById("highscores-list");
 
 goBackButton.addEventListener("click", function () {
     window.location.href = "./index.html";
 });
+
+var initialsList = localStorage.getItem("initialsList").split(",");
+var scoreList = localStorage.getItem("scoreList").split(",");
+
+var listElement;
+
+function addHighscore() {
+    for (var currentInitialsAndScore = 0; currentInitialsAndScore < initialsList.length-1; currentInitialsAndScore++) {
+        listElement = document.createElement("li");
+        listElement.innerText = initialsList[currentInitialsAndScore] + " - " + scoreList[currentInitialsAndScore];
+        highscoresList.appendChild(listElement);
+    }
+}
+
+addHighscore();
