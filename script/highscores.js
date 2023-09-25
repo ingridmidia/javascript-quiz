afd70babfe7709bf5a005ef11c577dce8218a03c
ingridmidia <ingridmidia@gmail.com>
2023-09-24 19:37:38 -0500

    added clear highscores functionality

--- script/highscores.js
+++ script/highscores.js
@@ -1,21 +1,28 @@
 var goBackButton = document.getElementById("go-back-button");
 var highscoresList = document.getElementById("highscores-list");
+var clearHighscoresButton = document.getElementById("clear-highscores-button");
 
 goBackButton.addEventListener("click", function () {
     window.location.href = "./index.html";
 });
 
-var initialsList = localStorage.getItem("initialsList").split(",");
-var scoreList = localStorage.getItem("scoreList").split(",");
-
-var listElement;
-
-function addHighscore() {
-    for (var currentInitialsAndScore = 0; currentInitialsAndScore < initialsList.length-1; currentInitialsAndScore++) {
+function showHighscore() {
+    if ((localStorage.getItem("initialsList") === null) && (localStorage.getItem("scoreList") === null)) {
+        return;
+    }
+    var initialsList = localStorage.getItem("initialsList").split(",");
+    var scoreList = localStorage.getItem("scoreList").split(",");
+    var listElement;
+    for (var currentInitialsAndScore = 0; currentInitialsAndScore < initialsList.length - 1; currentInitialsAndScore++) {
         listElement = document.createElement("li");
         listElement.innerText = initialsList[currentInitialsAndScore] + " - " + scoreList[currentInitialsAndScore];
         highscoresList.appendChild(listElement);
     }
 }
 
-addHighscore();
+showHighscore();
+
+clearHighscoresButton.addEventListener("click", function () {
+    localStorage.clear();
+    highscoresList.style.visibility = "hidden";
+});
